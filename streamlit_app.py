c10b5ea276eca68e32d64fa7c3faa57a790a82c4
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-12-11 14:31:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
-st.title(":blue[Projek LPK Kelas Nanoteknologi Pangan 2025"])
+st.title(":blue[Projek LPK Kelas Nanoteknologi Pangan 2025]")
 st.title("NTP :blue[cool] :sunglasses:")
