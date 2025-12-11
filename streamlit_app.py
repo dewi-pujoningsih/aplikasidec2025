25c1f5176891c2cd477efddd79069321aa9f3a71
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-12-11 14:18:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ st.title("Projek LPK Kelas Nanoteknologi Pangan 2025")
 st.title("===Penentuan bilangan ganjil atau genap===")
 number = st.number_input(
     "Masukkan angka dengan 0 desimal",
-    min_value=0.0,
-    max_value=10000.0,
-    format="%.1f"
+    min_value=0,
+    max_value=10000,
+    format="%.0f"
 )
 
 
