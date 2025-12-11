08eada448e14b50d7327e0f9cc7c6c18d27f287b
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-12-11 13:45:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,14 @@
 import streamlit as st
-with st.container():
-    st.write("This is inside the container")
-st.write("This is outside the container")
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
