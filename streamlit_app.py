7d6ddee6b1ba9ce72d6f43192fc637a6a9d3ca9a
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-12-11 15:32:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,4 @@
 import streamlit as st
 
-col1, col2, col3 = st.columns(3)
-
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
-
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
-
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+st.title(":green[Projek LPK 2025]")
+st.header(":blue[Penentuan Bilangan Ganjil dan Genap]")
