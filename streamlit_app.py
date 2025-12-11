62ff8fd043c926b2020cf00832a4032ec371e306
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-12-11 14:25:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,4 @@
 import streamlit as st
 
 st.title("Projek LPK Kelas Nanoteknologi Pangan 2025")
-st.title("===Penentuan bilangan ganjil atau genap===")
-number = st.number_input(
-    "Masukkan angka dengan 0 desimal",
-    min_value=0,
-    max_value=10000,
-    
-)
-
-
-#number = int(st.number_input("Masukkan sembarang angka:"))
-if number%2==1:
- st.write("Angka",number,"tergolong angka ganjil")
-else:
- st.write("Angka",number,"tergolong angka genap")
+st.title("NTP :blue[cool] :sunglasses:")
